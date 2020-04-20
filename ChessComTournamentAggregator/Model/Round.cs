5e0e156361fee0042419ab918072221634e11ee2
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2020-04-20 15:32:12 +0200

    Add support for Arena tournaments (#2)
    * Add support for Arena tournaments
    * Add Scores column

--- ChessComTournamentAggregator/Model/Round.cs
+++ ChessComTournamentAggregator/Model/Round.cs
@@ -7,5 +7,8 @@ namespace ChessComTournamentAggregator.Model
     {
         [JsonPropertyName("groups")]
         public ICollection<string> Groups { get; set; }
+
+        [JsonPropertyName("players")]
+        public ICollection<Player> Players { get; set; }
     }
 }
